fe108ab5f576c78768b8b43bb8903c75d42461cc
风度杰 <gjj492463556@gmail.com>
2021-02-04 17:15:14 +0800

    完成

---
+++ pages/duty/duty.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<!-- 设置圆角 -->
+		<uni-search-bar :radius="10" bgColor="#FFFFFF" :@confirm="search"></uni-search-bar>
+		
+		<mescroll-body ref="mescrollRef" @init="mescrollInit" :down="downOption" @down="downCallback" @up="upCallback">
+			<view class="notice">本Demo的下拉刷新: 添加新数据到列表顶部</view>
+			<view class="news-li" v-for="news in dataList" :key="news.id">
+				<view>{{news.title}}</view>
+				<view class="new-content">{{news.content}}</view>
+			</view>
+		</mescroll-body>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,95 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<map class="map" src="/static/logo.png"></map>
+		<text class="title">应急管理</text>
+
+		<view class="example-body">
+			<uni-grid :column="3" :showBorder="false" :square="false" :highlight="false" @change="change">
+				<uni-grid-item v-for="(item, index) in list" :index="index" :key="index">
+					<view class="grid-item-box">
+						<image :src="item.url" class="image" mode="aspectFill" />
+						<text class="text">{{ item.text }}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</view>
+		<text class="title">能源统计</text>
+		
+		<view class="example-body">
+			<uni-grid :column="2" :showBorder="false" :square="false" :highlight="false" @change="change">
+				<uni-grid-item v-for="(item, index) in listTwo" :index="index" :key="index">
+					<view class="grid-item-box">
+						<image :src="item.url" class="image" mode="aspectFill" />
+						<text class="text">{{ item.text }}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
 		</view>
 	</view>
 </template>
 
 <script>
+	import uniGrid from "@/components/uni-grid/uni-grid.vue"
+	import uniGridItem from "@/components/uni-grid-item/uni-grid-item.vue"
+
 	export default {
 		data() {
 			return {
 				title: 'Hello',
+				list: [{
+						url: '/static/logo.png',
+						text: 'Grid',
+
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 2',
+
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 3',
+
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 4',
+
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 5'
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 6'
+					}
+				],
+				listTwo: [{
+						url: '/static/logo.png',
+						text: 'Grid',
 				
+					},
+					{
+						url: '/static/logo.png',
+						text: 'Grid 2',
+				
+					}]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+			change() {
 
+			}
+		},
+		comments: {
+			components: {
+				uniGrid,
+				uniGridItem
+			}
 		}
 	}
 </script>
@@ -32,22 +102,51 @@
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
+	.map {
+		height: 300rpx;
+		width: 100%;
+		margin-top: 10rpx;
 		margin-left: auto;
 		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
+		margin-bottom: 10rpx;
 	}
 
 	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		width: 100%;
+		font-size: 30rpx;
+		margin-left: 15rpx;
+		color: #333333;
+	}
+
+	.example-body {
+		width: 95%;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		font-size: 14px;
+		background-color: #FFFFFF;
+		margin: 10rpx;
+		border: 2px solid #f9f9f9;
+		box-shadow: 0 0 25rpx #CCCCCC;
+	}
+
+	.image {
+		width: 50rpx;
+		height: 50rpx;
+	}
+
+	.text {
+		font-size: 26rpx;
+		margin-top: 10rpx;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
 	}
 </style>
